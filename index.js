d2c37f60ef348125d04a93c107f53336ffea8e92
Laurent Goderre <laurent.goderre@gmail.com>
2017-05-11 11:15:09 -0400

    Added logic to update the token in Travis

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var request = require('request');
 
 var ghUser = 'wet-boew-bot',
   ghPass = process.env.WET_BOT_PASS,
-  travisRootUrl = "https://api.travis-ci.org",
+  travisRootUrl = 'https://api.travis-ci.org',
   ghAuthOptions = {
     url: 'https://api.github.com/authorizations',
     auth: {
@@ -18,8 +18,14 @@ var ghUser = 'wet-boew-bot',
       'User-Agent': 'Travis; npm request',
       'Accept': 'application/vnd.travis-ci.2+json'
     },
+    json: true
   }
-  tokenMessage = "Test Token"//""Token for Pushing from Travis CI",
+  tokenId = 'GH_TOKEN'
+  tokenMessage = 'Token for Pushing from Travis CI',
+  repos = [
+    'wet-boew/wet-boew',
+    'wet-boew/theme-base'
+  ]
   checkToken = function(tokens) {
     var t, token;
 
@@ -34,44 +40,93 @@ var ghUser = 'wet-boew-bot',
   },
   deleteToken = function(tokenId, cb) {
     request(Object.assign({}, ghAuthOptions, {
-      method: "DELETE",
-      url: ghAuthOptions.url + "/" + tokenId
+      method: 'DELETE',
+      url: ghAuthOptions.url + '/' + tokenId
     }), function(error, response, body) {
-      console.log("Deleted token");
+      console.log('Deleted token');
       cb();
     })
   },
   createToken = function() {
     request(Object.assign({}, ghAuthOptions, {
-      method: "POST",
+      method: 'POST',
       json: true,
       body: {
         scopes: [
-          "repo"
+          'repo'
         ],
         note: tokenMessage
       }
     }), function(error, response, token) {
-      console.log("Created new token");
+      console.log('Created new token');
       authenticateTravis(token.token);
     });
   },
-  authenticateTravis = function(token) {
+  authenticateTravis = function() {
+    var token = process.env.GH_TOKEN;
     request(Object.assign({}, travisOptions, {
-      url: travisRootUrl + "/auth/github",
-      method: "POST",
-      json: true,
+      url: travisRootUrl + '/auth/github',
+      method: 'POST',
       body: {
         github_token: token
       }
     }), function(error, response, body) {
       var travisAuth = body.access_token;
-      console.log("Authenticated with Travis CI");
+      console.log('Authenticated with Travis CI');
       updateCITokens(token, travisAuth);
     });
-  }
-  updateCITokens = function(token, travisAUth) {
-    console.log("Updating CI with token " + token + "Travis Auth " + travisAUth)
+  },
+  getRepoTravisId = function(slug, cb) {
+    request(Object.assign({}, travisOptions, {
+      url: travisRootUrl + '/repos/' + slug
+    }), function(error, response, body) {
+      cb(body.repo.id);
+    });
+  },
+  getTravisEnvVarId = function(repoId, travisAuth, cb) {
+    var options = Object.assign({}, travisOptions, {
+      url: travisRootUrl + '/settings/env_vars?repository_id=' + repoId
+    });
+    options.headers.Authorization = 'token ' + travisAuth;
+    request(options, function(error, response, body) {
+      var e, env_var;
+      for (e = 0; e < body.env_vars.length; e++) {
+        env_var = body.env_vars[e];
+        if (env_var.name === "GH_TOKEN") {
+          cb(env_var.id);
+        }
+      }
+    });
+  },
+  updateTravisEnvVar = function(token, repoId, env_var_id, travisAuth, cb) {
+    var options = Object.assign({}, travisOptions, {
+      url: travisRootUrl + '/settings/env_vars/' + env_var_id + '?repository_id=' + repoId,
+      method: "PATCH",
+      body: {
+        env_var: {
+          value: token
+        }
+      }
+    });
+    options.headers.Authorization = 'token ' + travisAuth;
+    request(options, function(error, response, body) {
+      cb();
+    });
+  },
+  updateTravisRepoEnvVar = function(token, repo_slug, env_var, travisAuth) {
+    getRepoTravisId(repo_slug, function(repoId){
+      getTravisEnvVarId(repoId, travisAuth, function(env_var_id) {
+        updateTravisEnvVar(token, repoId, env_var_id, travisAuth, function() {
+          console.log("Updated token for repo " + repo_slug);
+        });
+      });
+    });
+  },
+  updateCITokens = function(token, travisAuth) {
+    for (var r = 0; r < repos.length; r++) {
+      updateTravisRepoEnvVar(token, repos[r], tokenId, travisAuth);
+    }
+
   };
 
 request(ghAuthOptions, function(error, response, body) {
